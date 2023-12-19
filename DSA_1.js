264b9afd2259003fe1eb80c1d6703b6e92c30a55
tejas <tejasnasre120@gmail.com>
2023-12-19 20:44:59 +0530

    first commit

---
+++ DSA_1.js
@@ -0,0 +1,30 @@
+// Checking Sum 0 - PS_1
+
+// [-5,-4,-3,-2,0,2,4,6,8] -> Input
+
+// [?,?] -> Output
+
+// [-4,4] -> Output
+
+//For In Loop Is Used To Print The Index Of Array
+    // for(let num in array){
+    //     console.log(num)
+    // }
+
+function getSumZeroPair(array){
+    //For Of Loop Is Used To Print The Array Element
+    for(let num of array){
+        // Another Loop For Comapring To Elements From The Array
+        for(let j=1; j<array.length; j++){
+            console.log(num,array[j])
+            if(num + array[j] === 0){
+                return [num,array[j]];
+            }
+        }
+    }
+}
+
+const resultPairZero = getSumZeroPair([-5,-4,-3,-2,0,2,4,6,8]);
+console.log(resultPairZero)
+
+// Time Complexity = O(n^2) Quadratic Time Complexityb
---
+++ DSA_1NEW.js
@@ -0,0 +1,21 @@
+// Same Question As First But We Want Single Iteration Loop For Linear Time Complexity
+
+// [-5,-4,-3,-2,0,2,4,6,8] -> Input
+
+function getZero(array){
+let left = 0;
+let right = array.length - 1;
+
+while(left < right){
+    let sum = array[left] + array[right];
+    if(sum == 0){
+        return [array[left].array[right]];
+    }
+    else if (sum > 0){
+        right--;
+    }
+    else{
+        left++; 
+    }
+}
+}
