1e722e4c358b99be3cde97f63e49719858445460
tejas <tejasnasre120@gmail.com>
2023-12-19 21:22:31 +0530

    getPairZero - LinearComplexity

--- DSA_1NEW.js
+++ DSA_1NEW.js
@@ -1,5 +1,7 @@
 // Same Question As First But We Want Single Iteration Loop For Linear Time Complexity
 
+// Linear Complexity getPairZero Question 1
+
 // [-5,-4,-3,-2,0,2,4,6,8] -> Input
 
 function getZero(array){
